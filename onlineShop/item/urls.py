402bba0fb2de04a648c84ffcccac2a5fedd418e3
m-stavrakov <131870370+m-stavrakov@users.noreply.github.com>
2024-05-06 14:36:31 +0100

    Error handling

--- onlineShop/item/urls.py
+++ onlineShop/item/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 app_name = 'item'
 
-url_patterns = [
+# IMPORTANT: urlpatterns should be spelled exactly like that
+urlpatterns = [
+    # this is a path; pk is primary key
     path('<int:pk>/', views.detail, name='detail'),
 ]
