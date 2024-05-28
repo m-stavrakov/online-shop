551ddc6fc2049cbf53ab04acd6204602377402af
m-stavrakov <131870370+m-stavrakov@users.noreply.github.com>
2024-05-28 14:55:09 +0100

    Final commit

--- onlineShop/onlineShop/urls.py
+++ onlineShop/onlineShop/urls.py
@@ -22,9 +22,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('admin/', admin.site.urls),
     path('dashboard/', include('dashboard.urls')),
+    path('inbox/', include('conversation.urls')),
     # this includes the urls.py from item folder
     path('items/', include('item.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
